60c8b451aa17c2fb6ad5d778fd740a2c14785dc0
Kichiyaki <xyztojajestem@gmail.com>
2021-11-11 08:19:14 +0100

    0.0.8

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,15 +1,19 @@
 exports.onRouteUpdate = function ({ location }) {
   if (
-    process.env.NODE_ENV === `production` &&
-    typeof window.plausible === `object`
+    process.env.NODE_ENV !== `production` ||
+    typeof window.plausible !== `object`
   ) {
-    const pathIsExcluded =
-      location &&
-      typeof window.plausibleExcludePaths !== `undefined` &&
-      window.plausibleExcludePaths.some((rx) => rx.test(location.pathname));
-
-    if (pathIsExcluded) return null;
-
-    window.plausible('pageview');
+    return;
   }
+
+  const pathIsExcluded =
+    location &&
+    typeof window.plausibleExcludePaths !== `undefined` &&
+    window.plausibleExcludePaths.some((rx) => rx.test(location.pathname));
+
+  if (pathIsExcluded) {
+    return null;
+  }
+
+  window.plausible('pageview');
 };
